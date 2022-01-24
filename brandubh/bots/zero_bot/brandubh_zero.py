Fix prior normalisation

brandubh/bots/zero_bot/brandubh_zero.py
@@ -57,8 +57,8 @@ class ZeroBot:
     def __init__(self, 
                  evals_per_turn = 140, 
                  batch_size = 7,
-                 c = 2.0,
-                 alpha = 0.03,
+                 c = 1.0,
+                 alpha = 0.15,
                  network = None):
         self.evals_per_turn = evals_per_turn
         self.alpha = alpha
@@ -107,6 +107,7 @@ class ZeroBot:
         if not self.root:
             self.root = self.create_root_node(game_state.copy())
         # self.root = self.create_root_node(game_state.copy())
+        # self.root.add_noise(self.alpha)
         
         
         # If no legal moves can be made from the given board position, pass 
@@ -133,7 +134,7 @@ class ZeroBot:
         # 2) Pick the move with the highest visit count.
         moves = [move for move in self.root.moves()]
         if moves:
-            if game_state.num_moves < 7:
+            if game_state.num_moves < 6:
                 p = np.asarray([self.root.branches[move].visit_count 
                                 for move in moves])
                 p = p/sum(p)
@@ -243,11 +244,13 @@ class ZeroBot:
         
         # If a root node is being created, then add some dirichlet noise
         # to the prior probabilities to help exploration.
-        if parent == None and self.alpha > 0:
-            num_branches = len(move_priors)
-            dirichlet_noise = np.random.dirichlet([self.alpha]*num_branches)
-            for (i, move) in enumerate(move_priors.keys()):
-                move_priors[move] = (move_priors[move] + dirichlet_noise[i])/2
+        # if parent == None and self.alpha > 0:
+        #     num_branches = len(move_priors)
+        #     # dirichlet_noise = np.random.dirichlet([self.alpha]*num_branches)
+        #     noise = np.random.gamma(self.alpha, 1, num_branches)
+        #     for (i, move) in enumerate(move_priors.keys()):
+        #         move_priors[move] = (0.75 * move_priors[move] + 
+        #                              0.25 * noise[i])
         
         # Create the node for the given game state, with the predicted value
         # and priors, and attach it to the tree.
@@ -469,8 +472,12 @@ class TreeNode:
         
         self.branches = {}
         moves = game_state.legal_moves()
+        N = 0
         for move in moves:
-            self.branches[move] = Branch(priors[move])
+            N += priors[move]
+            
+        for move in moves:
+            self.branches[move] = Branch(priors[move]/N)
                 
         self.children = {}
          
@@ -486,10 +493,18 @@ class TreeNode:
     def get_child(self, move):
         return self.children[move]
     
+    def add_noise(self, alpha):
+        if alpha > 0:
+            num_branches = len(self.branches)
+            noise = np.random.gamma(alpha, 1, num_branches)
+            N = sum(noise)
+            for i, branch in enumerate(self.branches.values()):
+                branch.prior = (0.75 * branch.prior + 0.25 * noise[i] / N)
+    
     def expected_value(self, move):
         branch = self.branches[move]
         if branch.visit_count == 0:
-            return -1
+            return 0
         return (branch.total_value + branch.virtual_loss) / branch.visit_count
     
     def increment_virtual_loss(self, move):
@@ -510,11 +525,13 @@ class TreeNode:
         # skewing the tree statistics slightly.
         if move:
             # Other PUCT scores will never be this negative
-            self.branches[move].virtual_loss = -7
+            self.branches[move].virtual_loss = -700
+            self.branches[move].visit_count = 1
         
     def unlock_branch(self, move):
         if move:
             self.branches[move].virtual_loss = 0
+            self.branches[move].visit_count = 0
     
     def prior(self, move):
         return self.branches[move].prior
@@ -633,6 +650,7 @@ class TreeClimber:
             move = node.last_move
             node = node.parent
             node.decrement_virtual_loss(move)
+            # TODO: explore using -0.9 here instead.
             value *= -1
             
         node.record_visit(move, value)
@@ -671,7 +689,12 @@ class TreeClimber:
                   
         Christopher D. Rosin - Multi-armed Bandits with Episode Context
         """
-        c_sqrt_total_n = self.c * np.sqrt(node.total_visit_count)
+        c_sqrt_total_n = np.sqrt(node.total_visit_count) #* self.c
+        
+        cb = 1400
+        ci = 1.4
+        c = np.log((1 + node.total_visit_count + cb)/cb) + ci
+        c_sqrt_total_n *= c
         
         def branch_score(move):
             q = node.expected_value(move)

brandubh/bots/zero_bot/networks/zero_network.py
@@ -139,12 +139,12 @@ class ZeroNet():
         # x = BatchNormalization(axis=1)(x)
         x = LeakyReLU()(x)
         x = Flatten()(x)
-        x = Dense(14, use_bias = biases, 
+        x = Dense(21, use_bias = biases, 
                   activation = 'linear',
                   bias_regularizer = l2(cls.alpha),
                    kernel_regularizer = l2(cls.alpha))(x)
         x = LeakyReLU()(x)
-        x = Dense(7, use_bias = biases, 
+        x = Dense(14, use_bias = biases, 
                   activation = 'linear',
                   bias_regularizer = l2(cls.alpha),
                   kernel_regularizer = l2(cls.alpha))(x)
@@ -156,7 +156,7 @@ class ZeroNet():
     @classmethod
     def policy_head(cls, x):
         biases = True
-        x = Conv2D(filters = 70, kernel_size = (3, 3), use_bias = biases,
+        x = Conv2D(filters = 70, kernel_size = (1, 1), use_bias = biases,
                    padding = 'same', 
                    activation = 'linear',
                    bias_regularizer = l2(cls.alpha),
@@ -221,13 +221,13 @@ class ZeroNet():
         move_priors = [self.encoder.decode_policy(p, pieces) 
                        for p, pieces in zip(priors, states_pieces)]
         
-        # Noramlise prior distributions
+        # Restrict and noramlise prior distributions over possible moves.
         for priors in move_priors:
             N = sum(priors.values())
             for (move, prior) in priors.items():
                 priors[move] /= N
         
-        predictions = [(priors, value) 
+        predictions = [(priors, value[0]) 
                        for priors, value in zip(move_priors, values)]
         return predictions
     
@@ -261,10 +261,10 @@ class ZeroNet():
         self.model = load_model(prefix + 'zero_model.h5')
         self.compile_lite_model()
         
-    def train(self, training_data, batch_size, epochs=350):
+    def train(self, training_data, batch_size, epochs=1):
         X, Y, rewards = training_data
         lr_schedule = self.get_lr_schedule()
-        loss = self.model.fit(X, [Y, rewards], 
+        loss = self.model.fit(X, [Y, rewards],
                               batch_size=batch_size, 
                               epochs=epochs,
                               callbacks=[self.stop_criteria, lr_schedule])
@@ -276,7 +276,7 @@ class ZeroNet():
         # if n < 70:
         #     schedule = lambda epoch, lr : (1/7)**(7)
         # else:
-        schedule = lambda epoch, lr : (1/7)**(4 + (n + epoch)//35000)
+        schedule = lambda epoch, lr : (1/7)**(4 + (n + epoch)//350)
         return LearningRateScheduler(schedule)
     
     
@@ -321,8 +321,8 @@ class ZeroNet():
             # moves between it and the winning move. Rewards for moves made by 
             # the winning side are positive and negative for the losing side.
             episode_rewards = episode['winner'] * np.array(episode['players'])
-            # episode_rewards = (np.exp(-1*(num_moves-np.arange(num_moves)-1)/40
-            #                          )) * episode_rewards
+            # episode_rewards = (np.exp(-1*(num_moves-np.arange(num_moves)-1)/28
+            #                           )) * episode_rewards
             
             rewards.append( episode_rewards )
             

brandubh/bots/zero_bot/zero_coach.py
@@ -41,12 +41,12 @@ from training_utils import gain_experience, save_training_data, DataManager, com
 from networks.zero_network import ZeroNet
 
 net = ZeroNet()
-bot = ZeroBot(evals_per_turn=350, batch_size=14, network=net)
+bot = ZeroBot(evals_per_turn=700, batch_size=21, network=net)
 
 def compile_bot(bot):
     bot.network.model.compile(optimizer=keras.optimizers.Adam(),
                               loss=['categorical_crossentropy', 'mse'],
-                              loss_weights=[1.0, 0.5])
+                              loss_weights=[1.0, 1.0])
     
 compile_bot(bot)
 
@@ -60,8 +60,8 @@ bot.save_as_old_bot()
 # %% train the bot
 import os
 
-num_episodes = 2
-num_cycles = 140
+num_episodes = 7
+num_cycles = 280
 
 move_limit = 140
 moves_to_look_ahead = 1
@@ -81,8 +81,8 @@ for cycle in range(num_cycles):
         bot.alpha = alpha_tmp
     
     print('\nGainning experience, cycle {0}'.format(cycle))
-    # eps = 1.0/(1.0 + (cycle+7)/7.0) # parameter for epsilon greedy selection.
-    eps = 0.07
+    eps = 1.0/(1.0 + (cycle+7)/7.0) + 0.035# parameter for epsilon greedy selection.
+    # eps = 0.07
     self_play_exp = gain_experience(bot, bot, num_episodes, move_limit, eps)
     # grand_wht_exp = gain_experience(gr_bot, bot, num_episodes, move_limit, 0)
     # grand_blk_exp = gain_experience(bot, gr_bot, num_episodes, move_limit, 0)
@@ -99,10 +99,11 @@ for cycle in range(num_cycles):
     dm.append_data(training_data)
     
     print('\nTraining network, cycle {0}'.format(cycle))
-    n = cycle + 1 if cycle < 7 else 7
+    # n = cycle + 1 if cycle < 7 else 7
+    n = 1
     for i in range(n):
-        training_data = dm.sample_training_data(2048)
-        losses = bot.network.train(training_data, batch_size=256)
+        training_data = dm.sample_training_data(4096)
+        losses = bot.network.train(training_data, batch_size=128)
     
     # lr = 0.00001/(1.0 + (cycle+1)/0.7)
     # compile_bot(bot)
@@ -110,6 +111,22 @@ for cycle in range(num_cycles):
         
     
     
+    
+# %% plot
+import matplotlib.pyplot as plt
+import numpy as np
+
+title = "22-1-2022-b"
+
+score_hist = [sc[0] for sc in bot.evaluation_history_ran]
+moving_av = np.convolve(score_hist, np.ones(14), 'valid')/14
+plt.plot(score_hist)
+plt.plot(range(6, len(score_hist)-7), moving_av)
+plt.title(title)
+plt.xlabel('iterations')
+plt.ylabel('average score')
+
+# plt.savefig('evaluation-'+title+'.png', dpi=300)
 
 # %% .
 from networks.zero_network import ZeroNet