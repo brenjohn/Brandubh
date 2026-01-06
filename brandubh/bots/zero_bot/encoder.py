a18ecaedb3d63546265149eba945bff432d030fd
brenjohn <johnmarkbrennan@gmail.com>
2026-01-06 20:07:27 +0000

    Rafactor ThreePlaneEncoder class

--- brandubh/bots/zero_bot/encoder.py
+++ brandubh/bots/zero_bot/encoder.py
@@ -5,7 +5,7 @@ Created on Sat Jan  3 16:04:57 2026
 
 @author: john
 
-This submodule defines a class used to encode game states as input tensors
+This submodule defines a classes used to encode game states as input tensors
 that can be passed to a ZeroBot neural network. It can also decode the output 
 of the network to a dictionary of move-value pairs.
 """
@@ -13,63 +13,14 @@ of the network to a dictionary of move-value pairs.
 import numpy as np
 
 
-class SixPlaneEncoder():
-    """This class is used to encode a brandubh game state as a tensor with six
-    channels (See encode method) and is intended to be used with the ZeroNet
-    class.
+class BaseEncoder:
+    """A base class that other encoder classes should inherit from.
     """
         
     def encode(self, game_state):
+        """Encode the given game state as a tensor.
         """
-        A game state is encoded as six 7x7 planes (or a tensor with shape 
-        (7,7,6)) as described below.
-        
-        The first 7x7 plane is an array of 0's and 1's encoding the positions 
-        of all pieces owned by the current player, i.e. the array has a 1 in 
-        entries corresponding to squares of the board occupied by one of the 
-        current players pieces and a 0 otherwise.
-        
-        The second 7x7 plane is an array encoding the position of king piece 
-        owned by the current player. If the player doesn't own a king then 
-        this plane will be all zeros, otherwise it will have a 1 in the 
-        relevant entry.
-        
-        The next two planes are the same as the first two but of the opposite 
-        player.
-        
-        The fifth plane is a 7x7 array of 1's if the current player is playing 
-        as white and is all 0's otherwise.
-        
-        The sixth plane is a 7x7 array of 1's if the current player is playing 
-        as black and is all 0's otherwise.
-        """
-        board_tensor = np.zeros((7,7,6))
-        player       = game_state.player
-        game_set     = game_state.game_set
-        
-        if player == 1:
-            board_tensor[:, :, 4] = 1
-        else:
-            board_tensor[:, :, 5] = 1
-        
-        # white soldier pieces
-        for piece in range(1, 11, 2):
-            r, c = game_set.piece_position(piece)
-            if r > -1:
-                board_tensor[r, c, 1 - player] = 1
-            
-        # King piece
-        r, c = game_set.piece_position(1)
-        if r > -1:
-            board_tensor[r, c, 2 - player] = 1
-        
-        # black soldier pieces
-        for piece in range(2, 18, 2):
-            r, c = game_set.piece_position(piece)
-            if r > -1:
-                board_tensor[r, c, 1 + player] = 1
-            
-        return board_tensor
+        pass
     
     
     def decode_policy(self, model_output, legal_moves):
@@ -178,3 +129,136 @@ class SixPlaneEncoder():
         X = np.concatenate(X)
         Y = np.concatenate(Y)
         return X, Y, rewards
+    
+
+
+class SixPlaneEncoder(BaseEncoder):
+    """This class is used to encode a brandubh game state as a tensor with six
+    channels (See encode method) and is intended to be used with the ZeroNet
+    class.
+    """
+        
+    def encode(self, game_state):
+        """
+        A game state is encoded as six 7x7 planes (or a tensor with shape 
+        (7,7,6)) as described below.
+        
+        The first 7x7 plane is an array of 0's and 1's encoding the positions 
+        of all pieces owned by the current player, i.e. the array has a 1 in 
+        entries corresponding to squares of the board occupied by one of the 
+        current players pieces and a 0 otherwise.
+        
+        The second 7x7 plane is an array encoding the position of king piece 
+        owned by the current player. If the player doesn't own a king then 
+        this plane will be all zeros, otherwise it will have a 1 in the 
+        relevant entry.
+        
+        The next two planes are the same as the first two but of the opposite 
+        player.
+        
+        The fifth plane is a 7x7 array of 1's if the current player is playing 
+        as white and is all 0's otherwise.
+        
+        The sixth plane is a 7x7 array of 1's if the current player is playing 
+        as black and is all 0's otherwise.
+        """
+        board_tensor = np.zeros((7,7,6))
+        player       = game_state.player
+        game_set     = game_state.game_set
+        
+        if player == 1:
+            board_tensor[:, :, 4] = 1
+        else:
+            board_tensor[:, :, 5] = 1
+        
+        # white soldier pieces
+        for piece in range(1, 11, 2):
+            r, c = game_set.piece_position(piece)
+            if r > -1:
+                board_tensor[r, c, 1 - player] = 1
+            
+        # King piece
+        r, c = game_set.piece_position(1)
+        if r > -1:
+            board_tensor[r, c, 2 - player] = 1
+        
+        # black soldier pieces
+        for piece in range(2, 18, 2):
+            r, c = game_set.piece_position(piece)
+            if r > -1:
+                board_tensor[r, c, 1 + player] = 1
+            
+        return board_tensor
+
+
+
+class ThreePlaneEncoder(BaseEncoder):
+    """This class is used to encode a brandubh game state as a tensor with 
+    three channels (See encode method) and is intended to be used with the
+    DualNet class.
+    """
+        
+    def encode(self, game_state):
+        """
+        A game state is encoded as three 7x7 planes (or a tensor with shape 
+        (7,7,3)) as described below.
+        
+        The first 7x7 plane is an array of 0's and 1's encoding the positions 
+        of all soldier pieces owned by the white player, i.e. the array has a 
+        1 in entries corresponding to squares of the board occupied by a white
+        soldier.
+        
+        The second 7x7 plane is an array encoding the position of king piece.
+        
+        The third plane encodes the position of all the black pieces.
+        """
+        board_tensor = np.zeros((7,7,3))
+        game_set = game_state.game_set
+            
+        # white soldier pieces
+        for piece in range(3, 11, 2):
+            r, c = game_set.piece_position(piece)
+            board_tensor[r, c, 0] = 1
+            
+        # King piece
+        r, c = game_set.piece_position(1)
+        board_tensor[r, c, 1] = 1
+        
+        # black soldier pieces
+        for piece in range(2, 18, 2):
+            r, c = game_set.piece_position(piece)
+            board_tensor[r, c, 2] = 1
+        
+        return board_tensor
+    
+    
+    def create_training_data(self, experience):
+        white_experience, black_experience = self.split_experience(experience)
+        white_training_data = super().create_training_data(white_experience)
+        black_training_data = super().create_training_data(black_experience)
+        return white_training_data, black_training_data
+    
+    
+    def split_experience(self, experience):
+        white_experience, black_experience = [], []
+        for episode in experience:
+            white_turns, black_turns = self.split_episode(episode)
+            white_experience.append(white_turns)
+            black_experience.append(black_turns)
+        return white_experience, black_experience
+    
+    
+    def split_episode(self, episode):
+        white_turns = [i for i, p in enumerate(episode['players']) if p == 1]
+        black_turns = [i for i, p in enumerate(episode['players']) if p == -1]
+        
+        white_data, black_data = {}, {}
+        for key, value in episode.items():
+            if isinstance(value, list):
+                white_data[key] = [value[i] for i in white_turns]
+                black_data[key] = [value[i] for i in black_turns]
+            elif isinstance(value, int):
+                white_data[key] = value
+                black_data[key] = value
+        
+        return white_data, black_data
